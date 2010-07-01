a1a16af69c4cda6dd98a7b97dc850c920b937c36
tangrui <tangrui@zyeeda.com>
2010-07-01 22:13:16 +0800

    Fix unit test bug.

--- src/test/java/com/zyeeda/framework/unittest/TestServer.java
+++ src/test/java/com/zyeeda/framework/unittest/TestServer.java
@@ -4,14 +4,17 @@ import org.apache.commons.configuration.Configuration;
 
 import com.zyeeda.framework.services.Server;
 import com.zyeeda.framework.services.ServiceInvocationHandler;
+import com.zyeeda.framework.services.TemplateService;
 import com.zyeeda.framework.services.impl.TemplateServiceImpl;
 
 public class TestServer extends Server {
 
+	@Override
 	public void init(Configuration config) throws Exception {
 		super.init(config);
 		
-		this.addService(new ServiceInvocationHandler<TemplateServiceImpl>().bind(new TemplateServiceImpl(this)));
+		TemplateService tplSvc = new TemplateServiceImpl(this);
+		this.addService(new ServiceInvocationHandler<TemplateService>().bind(tplSvc));
 		//this.addService(new PersistenceService(this));
 	}
 }
