4518d4c5c1def5e8cb82b515e1e13ade24457221
zhaoqi <devnull@localhost>
2010-07-01 21:55:21 +0800

    add service interface

---
+++ src/main/java/com/zyeeda/framework/services/TemplateService.java
@@ -0,0 +1,13 @@
+package com.zyeeda.framework.services;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+import freemarker.template.TemplateException;
+
+public interface TemplateService extends Service {
+
+	public void paint(String tplPath, Writer out, Map<?, ?> args) throws IOException, TemplateException;
+	
+}
