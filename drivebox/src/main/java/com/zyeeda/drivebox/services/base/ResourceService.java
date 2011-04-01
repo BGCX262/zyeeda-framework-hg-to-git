371062fef6015374ac3223b5dac4afa53865b3bb
tangrui <tangrui@zyeeda.com>
2011-04-01 21:56:40 +0800

    Add getSecurityService to ResourceService. Issue 4

--- drivebox/src/main/java/com/zyeeda/drivebox/services/base/ResourceService.java
+++ drivebox/src/main/java/com/zyeeda/drivebox/services/base/ResourceService.java
@@ -7,6 +7,8 @@ import org.apache.tapestry5.ioc.Registry;
 import com.zyeeda.framework.FrameworkConstants;
 import com.zyeeda.framework.persistence.PersistenceService;
 import com.zyeeda.framework.persistence.internal.DefaultPersistenceServiceProvider;
+import com.zyeeda.framework.security.SecurityService;
+import com.zyeeda.framework.security.internal.ShiroSecurityServiceProvider;
 import com.zyeeda.framework.utils.IocUtils;
 
 public class ResourceService {
@@ -29,6 +31,10 @@ public class ResourceService {
 	
 	protected PersistenceService getPersistenceService() {
 		return this.reg.getService(IocUtils.getServiceId(DefaultPersistenceServiceProvider.class), PersistenceService.class);
+	}
+	
+	protected SecurityService<?> getSecurityService() {
+		return this.reg.getService(IocUtils.getServiceId(ShiroSecurityServiceProvider.class), SecurityService.class);
 	}
 	
 }
