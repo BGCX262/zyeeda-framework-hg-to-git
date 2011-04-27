84634fbcb2243dc70e0e1d7e068abf43bfa529fd
zhufang <zhufang@zyeeda.com>
2011-04-27 22:05:03 +0800

    Add mongodb document service and manager.

--- core/src/main/java/com/zyeeda/framework/entities/Document.java
+++ core/src/main/java/com/zyeeda/framework/entities/Document.java
@@ -1,6 +1,7 @@
 package com.zyeeda.framework.entities;
 
 import java.sql.Blob;
+import java.util.Date;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -8,55 +9,149 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import java.io.*;
 
 import com.zyeeda.framework.entities.base.RevisionDomainEntity;
+import com.zyeeda.framework.entities.base.SimpleDomainEntity;
 
 @Entity
 @Table(name = "ZDA_SYS_DOCUMENTS")
-public class Document extends RevisionDomainEntity {
+public class Document extends SimpleDomainEntity {
 
 	private static final long serialVersionUID = -5913731949268189623L;
 	
 	private String foreignId;
 	private int weight;
 	private String owner;
-	private Blob content;
-	
-	@Basic
-	@Column(name = "F_FOREIGN_ID")
+	private String creator;
+
+
+	private Date createdTime;
+    private String lastModifier;
+    private Date lastModifiedTime;
+    private long fileSize;
+    public long getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(long fileSize) {
+		this.fileSize = fileSize;
+	}
+
+	private String fileType; 
+    private String keyword;
+    private InputStream content;
+    private String contentType;
+    private String subType;
+    private String type;
+    
+    
+    
+    
+    public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+
+
+	public Document() {
+    	Date now = new Date();
+    	this.createdTime = now;
+    	this.lastModifiedTime = now;
+    }
+    
 	public String getForeignId() {
-		return this.foreignId;
+		return foreignId;
 	}
 	public void setForeignId(String foreignId) {
 		this.foreignId = foreignId;
 	}
-	
-	@Basic
-	@Column(name = "F_WEIGHT")
 	public int getWeight() {
-		return this.weight;
+		return weight;
 	}
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
-	
-	@Basic
-	@Column(name = "F_OWNER")
 	public String getOwner() {
-		return this.owner;
+		return owner;
 	}
 	public void setOwner(String owner) {
 		this.owner = owner;
 	}
-	
-	@Basic(fetch = FetchType.LAZY)
-	@Lob
-	@Column(name = "F_CONTENT")
-	public Blob getContent() {
-		return this.content;
+	public String getCreator() {
+		return creator;
 	}
-	public void setContent(Blob content) {
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public Date getCreatedTime() {
+		return createdTime;
+	}
+	public void setCreatedTime(Date createdTime) {
+		this.createdTime = createdTime;
+	}
+	public String getLastModifier() {
+		return lastModifier;
+	}
+	public void setLastModifier(String lastModifier) {
+		this.lastModifier = lastModifier;
+	}
+	public Date getLastModifiedTime() {
+		return lastModifiedTime;
+	}
+	public void setLastModifiedTime(Date lastModifiedTime) {
+		this.lastModifiedTime = lastModifiedTime;
+	}
+	
+
+	public String getKeyword() {
+		return this.keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Transient
+	public InputStream getContent() {
+		return content;
+	}
+	
+	public void setContent(InputStream content) {
 		this.content = content;
 	}
+    
+    public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+
+	public String getSubType() {
+		return subType;
+	}
+
+	public void setSubType(String subType) {
+		this.subType = subType;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 
 }
--- core/src/main/java/com/zyeeda/framework/managers/DocumentManager.java
+++ core/src/main/java/com/zyeeda/framework/managers/DocumentManager.java
@@ -1,8 +1,28 @@
 package com.zyeeda.framework.managers;
 
-import com.googlecode.genericdao.dao.jpa.GenericDAO;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.List;
+
 import com.zyeeda.framework.entities.Document;
 
-public interface DocumentManager extends GenericDAO<Document, String> {
+public interface DocumentManager {
+
+	// 创建
+	public void persist(Document doc) throws DocumentException, IOException;
+
+	// 根据ID名字去查询
+	public Document findByIdAndFileName(String id, String fileName)
+			throws DocumentException;
+
+	// 根据拥有人关键字数据类别去查询
+	public List<Document> findByKeyword(String owner, String keyword,
+			String foreignId, int skip, int limit) throws DocumentException;
+
+	public void updateDocument(Document document) throws UnknownHostException,
+			DocumentException;
+
+	public void removeById(String id) throws UnknownHostException,
+			DocumentException;
 
 }
