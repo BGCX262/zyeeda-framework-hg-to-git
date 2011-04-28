3a062eb702fb46523ccc2ef8c22fa67f5f29b6fc
dengjiu <dengjiu@zyeeda.com>
2011-04-28 10:25:30 +0800

    submit role

---
+++ core/src/main/java/com/zyeeda/framework/managers/RoleManager.java
@@ -0,0 +1,8 @@
+package com.zyeeda.framework.managers;
+
+import com.googlecode.genericdao.dao.jpa.GenericDAO;
+import com.zyeeda.framework.entities.Role;
+
+public interface RoleManager extends GenericDAO<Role, String> {
+
+}
