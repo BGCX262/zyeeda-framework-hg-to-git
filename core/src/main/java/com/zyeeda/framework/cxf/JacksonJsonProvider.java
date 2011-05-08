5fd855f55777b2ecb0dc9521bee1ee87e27eae17
tangrui <tangrui@zyeeda.com>
2011-05-08 23:39:34 +0800

    Make CXF serialize date type in pre-defined format.

--- core/src/main/java/com/zyeeda/framework/cxf/JacksonJsonProvider.java
+++ core/src/main/java/com/zyeeda/framework/cxf/JacksonJsonProvider.java
@@ -1,7 +1,10 @@
 package com.zyeeda.framework.cxf;
 
+import java.text.SimpleDateFormat;
+
 import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.map.SerializationConfig.Feature;
 
@@ -12,8 +15,15 @@ public class JacksonJsonProvider extends JacksonJaxbJsonProvider {
 		if (m == null) {
 			m = this._mapperConfig.getDefaultMapper();
 		}
+		SerializationConfig sc = m.getSerializationConfig().withDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+		sc.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+		sc.set(Feature.FAIL_ON_EMPTY_BEANS, false);
+		m.setSerializationConfig(sc);
+		/*
 		m.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
 		m.configure(Feature.FAIL_ON_EMPTY_BEANS, false);
+		m.setSerializationConfig(m.getSerializationConfig().withDateFormat(new SimpleDateFormat("yyyy-MM-dd")));
+		*/
 	}
 	
 }
