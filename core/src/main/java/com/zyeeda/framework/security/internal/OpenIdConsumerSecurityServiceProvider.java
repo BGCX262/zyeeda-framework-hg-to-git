8ed5f4b8c1f6b20937bc38f74cec52e0465afb8f
tangrui <tangrui@zyeeda.com>
2011-05-20 19:07:27 +0800

    add ioc annotations to OpenIdConsumerSecurityServiceProvider

--- core/src/main/java/com/zyeeda/framework/security/internal/OpenIdConsumerSecurityServiceProvider.java
+++ core/src/main/java/com/zyeeda/framework/security/internal/OpenIdConsumerSecurityServiceProvider.java
@@ -3,11 +3,16 @@ package com.zyeeda.framework.security.internal;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
+import org.apache.tapestry5.ioc.annotations.Marker;
+import org.apache.tapestry5.ioc.annotations.Primary;
+import org.apache.tapestry5.ioc.annotations.ServiceId;
 import org.apache.tapestry5.ioc.services.RegistryShutdownHub;
 
 import com.zyeeda.framework.security.SecurityService;
 import com.zyeeda.framework.service.AbstractService;
 
+@Marker(Primary.class)
+@ServiceId("openid-consumer-security-service")
 public class OpenIdConsumerSecurityServiceProvider extends AbstractService implements SecurityService<SecurityManager> {
 
 	public OpenIdConsumerSecurityServiceProvider(RegistryShutdownHub shutdownHub) {
