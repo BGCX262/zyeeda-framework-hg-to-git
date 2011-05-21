c620af9876cc38d0295f52cb7d40cbaf985f6a7c
yangjie191191 <yangjie191191@gmail.com>
2011-05-21 13:29:36 +0800

    update user sync.

--- core/src/main/java/com/zyeeda/framework/synch/UserSynchService.java
+++
@@ -1,9 +0,0 @@
-package com.zyeeda.framework.synch;
-
-import com.zyeeda.framework.entities.User;
-
-public interface UserSynchService {
-	public void persist(User user);
-	public void update(User user);
-	public void setVisible(Boolean visible, String ...ids);
-}
