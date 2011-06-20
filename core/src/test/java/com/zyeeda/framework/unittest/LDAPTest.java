5a79ade6ae0a1fa278ae30c1b1d50032daf73bea
chenhaifeng <chenhaifeng@zyeeda.com>
2011-06-20 10:30:20 +0800

    add search root org method

--- core/src/test/java/com/zyeeda/framework/unittest/LDAPTest.java
+++ core/src/test/java/com/zyeeda/framework/unittest/LDAPTest.java
@@ -28,7 +28,7 @@ public class LDAPTest {
 		String root = "dc=ehv,dc=csg,dc=cn";
 		Hashtable<String, String> env = new Hashtable<String, String>();
 		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-		env.put(Context.PROVIDER_URL, "ldap://localhost:389/" + root);
+		env.put(Context.PROVIDER_URL, "ldap://192.168.1.123:389/" + root);
 		env.put(Context.SECURITY_AUTHENTICATION, "simple");
 		env.put(Context.SECURITY_PRINCIPAL, "cn=admin");
 		env.put(Context.SECURITY_CREDENTIALS, "admin");
@@ -203,7 +203,8 @@ System.out.println(entry.getName());
 //		System.exit(0);
 //		save();
 //		saveUserRefObject();
-		ldapPageView();
+//		ldapPageView();
+		sort();
 	}
 
 }
