be55d0e595aaa5f58d0d3614f9e847193c338526
chenhaifeng <chenhaifeng@zyeeda.com>
2011-06-20 15:31:00 +0800

    modify merge error

--- core/src/main/java/com/zyeeda/framework/ws/base/ResourceService.java
+++ core/src/main/java/com/zyeeda/framework/ws/base/ResourceService.java
@@ -66,7 +66,4 @@ public class ResourceService {
 		return this.reg.getService(KnowledgeService.class);
 	}
 	
-	protected MongoDbService getMongoDbService() {
-		return this.reg.getService(IocUtils.getServiceId(DefaultMongoDbServiceProvider.class), MongoDbService.class);
-	}
 }
