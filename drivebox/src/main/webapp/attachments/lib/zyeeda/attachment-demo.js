64ccec92465ea1e83f97d60ade04898987035a87
tangrui <tangrui@zyeeda.com>
2011-06-23 22:33:06 +0800

    finish document service backend and demo

--- drivebox/src/main/webapp/attachments/lib/zyeeda/attachment-demo.js
+++ drivebox/src/main/webapp/attachments/lib/zyeeda/attachment-demo.js
@@ -9,6 +9,20 @@ ZYEEDA.namespace('com.zyeeda.zui.framework.demo').AttachmentDemo = function() {
                 foreignId : 'tangrui'
             });
             attach.render('#attachment');
+
+            var uploadedList = new Z.AttachmentUploadedList({
+                listUrl : Z.cfg.uploader.listUrl,
+                foreignId : 'zhaoqi',
+                width : '800px',
+                height : '335px',
+                readOnly : true
+            });
+            uploadedList.render('#manager');
+
+            Z.one('#destroyBtn').on('click', function() {
+                uploadedList.destroy();
+                attach.destroy();
+            });
         }
     }
 
