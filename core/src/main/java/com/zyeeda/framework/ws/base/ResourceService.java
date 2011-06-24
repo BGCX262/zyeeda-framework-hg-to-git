ef729a37a1cb2709c9d7f018837270788c28f246
[chenhaifeng <[chenhaifeng@zyeeda.com]chenhaifeng>
2011-06-24 09:32:05 +0800

    merg branch 07b725963ffd.

--- core/src/main/java/com/zyeeda/framework/ws/base/ResourceService.java
+++ core/src/main/java/com/zyeeda/framework/ws/base/ResourceService.java
@@ -62,4 +62,7 @@ public class ResourceService {
 		return this.reg.getService(KnowledgeService.class);
 	}
 
+	protected MongoDbService getMongoDbService() {
+		return this.reg.getService(IocUtils.getServiceId(DefaultMongoDbServiceProvider.class), MongoDbService.class);
+	}
 }
