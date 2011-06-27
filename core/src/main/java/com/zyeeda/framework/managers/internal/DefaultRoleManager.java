a5e1e142165c77548e06ea4367822df0950aeaad
chenhaifeng <chenhaifeng@zyeeda.com>
2011-06-27 10:25:04 +0800

    修改merge后的代码错误

--- core/src/main/java/com/zyeeda/framework/managers/internal/DefaultRoleManager.java
+++ core/src/main/java/com/zyeeda/framework/managers/internal/DefaultRoleManager.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import javax.persistence.TypedQuery;
 
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,39 +32,6 @@ public class DefaultRoleManager extends DomainEntityManager<Role, String>
 		return roleList;
 	}
 
-	
-	@SuppressWarnings("unchecked")
-	public List<Role> getRoleDistinct(String hql){
-		List<Role> list = new ArrayList<Role>();
-		TypedQuery<Role> createNativeQuery = (TypedQuery<Role>) this.em().createNativeQuery(hql);
-		TypedQuery<Role> query = createNativeQuery;
-		 list = query.getResultList();
-		return list;
-	}
-	
-	public List<RoleVo> roleToVo(List<Role> listRole) {
-		List<RoleVo> listRoleVo = new ArrayList<RoleVo>();
-		for(Role role : listRole) {
-			RoleVo roleVo = new RoleVo();
-			if(role.getDeptepment() == null && role.getDeptepmentId() == null){
-				roleVo.setCheckName(role.getName());
-				roleVo.setLabel(role.getName());
-				roleVo.setId(role.getId());
-				roleVo.setLeaf(true);
-				roleVo.setType("tesk");
-				roleVo.setKind("user");
-			} else {
-				roleVo.setId(role.getDeptepmentId());
-				roleVo.setCheckName(role.getDeptepment());
-				roleVo.setLabel(role.getDeptepment());
-				roleVo.setIo("/rest/line_location/"+role.getDeptepmentId());
-				roleVo.setLeaf(false);
-				roleVo.setType("io");
-			}
-			listRoleVo.add(roleVo);
-		}
-		return listRoleVo;
-	}
 
 	
 	@SuppressWarnings("unchecked")
