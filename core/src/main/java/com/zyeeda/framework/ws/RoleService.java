0fd183a1e7046d099f73e7527d193fa6ebeb3c0f
dengjiu <dengjiu2030@zyeeda.com>
2011-06-27 11:10:16 +0800

    update RoleService.java

--- core/src/main/java/com/zyeeda/framework/ws/RoleService.java
+++ core/src/main/java/com/zyeeda/framework/ws/RoleService.java
@@ -337,7 +337,7 @@ public class RoleService extends ResourceService{
 		//List<String> subjectList = new ArrayList<String>();
 		Set<UserVo> userNameVoList = new HashSet<UserVo>();
 		for(Role role : roleList){
-			if("当班值-值长".equals(role.getName())){
+			//if("当班值-值长".equals(role.getName()) && role.getDeptepment()){
 				for(String user : role.getSubjects()){
 					UserVo userVo = new UserVo();
 					userVo.setCheckName(user);
@@ -355,7 +355,7 @@ public class RoleService extends ResourceService{
 						}
 					}
 				}
-			}
+			//}
 		}
 		return userNameVoList;
 	}
